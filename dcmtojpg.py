f6306181f82fc5834ae96a4b07acbf679b0ae4ea
shashwat sahni <30469674+sahni15@users.noreply.github.com>
2020-12-08 21:29:57 +0530

    Update dcmtojpg.py

--- dcmtojpg.py
+++ dcmtojpg.py
@@ -4,6 +4,38 @@ import pydicom
 from PIL import Image
 from flask import Flask, render_template, request
 
+def dicomtojpg(file):
+    # file is the uploaded file
+    try:
+        ds = pydicom.dcmread(file)
+        shape = ds.pixel_array.shape
+
+        # Convert to float
+        image_2d = ds.pixel_array.astype(float)
+
+        # Rescaling and normalising
+        image_2d_scaled = (np.maximum(image_2d, 0) / image_2d.max()) * 255.0
+
+        # Convert to uint
+        image_2d_scaled = np.uint8(image_2d_scaled)
+
+        # save numpy array to a jpeg image
+        im = Image.fromarray(image_2d_scaled)
+
+        target_img = os.path.join(APP_ROOT, 'static/images/')
+
+        if not os.path.isdir(target_img):
+            os.mkdir(target_img)
+
+        name = os.path.basename(file)
+        destination = os.path.join(target_img, name.replace('.dcm', '.jpeg'))
+        im.save(destination, "JPEG")
+    except:
+        print('Could not convert: ', file)
+
+
+
+        
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -38,33 +70,3 @@ def upload():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-def dicomtojpg(file):
-    # file is the uploaded file
-    try:
-        ds = pydicom.dcmread(file)
-        shape = ds.pixel_array.shape
-
-        # Convert to float
-        image_2d = ds.pixel_array.astype(float)
-
-        # Rescaling and normalising
-        image_2d_scaled = (np.maximum(image_2d, 0) / image_2d.max()) * 255.0
-
-        # Convert to uint
-        image_2d_scaled = np.uint8(image_2d_scaled)
-
-        # save numpy array to a jpeg image
-        im = Image.fromarray(image_2d_scaled)
-
-        target_img = os.path.join(APP_ROOT, 'static/images/')
-
-        if not os.path.isdir(target_img):
-            os.mkdir(target_img)
-
-        name = os.path.basename(file)
-        destination = os.path.join(target_img, name.replace('.dcm', '.jpeg'))
-        im.save(destination, "JPEG")
-    except:
-        print('Could not convert: ', file)
